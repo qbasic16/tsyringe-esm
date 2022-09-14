f1138806d29c32bed4832001e00416c1a9e39f78
Peter Janser <pj@janser-it.ch>
2022-09-14 19:38:54 +0200

    Change typeInfo storage

--- dist/typings/reflection-helpers.d.ts
+++ dist/typings/reflection-helpers.d.ts
@@ -3,8 +3,9 @@ import InjectionToken from "./providers/injection-token";
 import { ParamInfo } from "./dependency-container";
 import Transform from "./types/transform";
 export declare const INJECTION_TOKEN_METADATA_KEY = "injectionTokens";
-export declare function getParamInfo( target: constructor<any> ): ParamInfo[];
-export declare function defineInjectionTokenMetadata( data: any, transform?: {
+export declare const PARAM_INFOS_METADATA_KEY = "paramInfos";
+export declare function getParamInfo(target: constructor<any>): ParamInfo[];
+export declare function defineInjectionTokenMetadata(data: any, transform?: {
     transformToken: InjectionToken<Transform<any, any>>;
     args: any[];
-} ): ( target: any, propertyKey: string | symbol, parameterIndex: number ) => any;
+}): (target: any, propertyKey: string | symbol, parameterIndex: number) => any;

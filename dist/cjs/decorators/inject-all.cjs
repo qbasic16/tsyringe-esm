08639ea2e68258025838eecfad0dab7d229f2fb3
Peter Janser <pj@janser-it.ch>
2022-09-15 08:50:55 +0200

    Remove CJS

--- dist/cjs/decorators/inject-all.cjs
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const reflection_helpers_1 = require("../reflection-helpers");
-function injectAll(token) {
-    const data = { token, multiple: true };
-    return (0, reflection_helpers_1.defineInjectionTokenMetadata)(data);
-}
-exports.default = injectAll;
--- dist/cjs/decorators/inject.cjs
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const reflection_helpers_1 = require("../reflection-helpers");
-function inject(token) {
-    return (0, reflection_helpers_1.defineInjectionTokenMetadata)(token);
-}
-exports.default = inject;
